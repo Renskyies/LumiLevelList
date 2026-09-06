9acef1ee5822749be105136ef2961c1fe0b18092
Renskyies <renskyzx@gmail.com>
2026-09-06 18:45:04 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -106,13 +106,7 @@ export default {
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
-                    </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        Send a screenshot/recording of the completion on the formulary
                     </p>
                     <p>
                         Do not use secret routes or bug routes
